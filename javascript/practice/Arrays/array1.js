3b11ded3fd81da000bdfd7dcdcc7213bc0b666df
Eric-Alves1 <ericalves.429634@gmail.com>
2026-01-13 09:42:09 -0300

    learning what arrays are

---
+++ javascript/practice/Arrays/array1.js
@@ -0,0 +1,50 @@
+//creating arrays skills and goals
+let skills = [
+    "Html", 
+    " Css", 
+    " JavaScript", 
+    " Git", 
+    " Github", 
+    " English"
+  ];
+
+let goals = [
+    "Have a laptop",
+    "become a full stack developer",
+    "get a remote job",
+    "travel around the world while working"
+  ];
+
+//modifying the array goals
+goals.push("get a international job", "make my mom happy", "become a good man");
+goals[0] = "get a strong body";
+goals.unshift("eat healthy")
+goals.pop()
+console.log(`i have ${skills.length} skills and ${goals.length} goals.`);
+console.log(`but my main goal is ${goals[6]}`);
+
+//modifying the array skills
+console.log(`my skills were ${skills}`)
+const skillRemoved = skills.splice(3, 6)
+console.log(`but now they are ${skills}, because in my opinion knowing ${skillRemoved} is the minimum`)
+
+//accesses specifics items inside a multidimensional array
+const random = ["three", 2, 1, [4, 8, 7, [5, 6]]];
+console.log(random[3][3][0]);
+
+//knowing the index of a specific item in an big array
+const birds = [
+    "sparrow",
+    "canary",
+    "hummingbird",
+    "swallow",
+    "thrush",
+    "owl",
+    "falcon",
+    "seagull",
+    "pigeon",
+    "toucan"
+  ];
+
+console.log(`the bird owl is at the position ${birds.indexOf("owl")}`);
+console.log(birds[5]);
---
+++ javascript/practice/Arrays/array2.js
@@ -0,0 +1,48 @@
+const foods = [
+    "beans",
+    "rice",
+    "meat",
+    "eggs",
+    "milk",
+    "oil",
+    "bread",
+    "oats",
+    "bananas"
+ ]
+ 
+ for (const food of foods) {
+   console.log(food);
+ }
+ 
+ let numberString = ["10", "20", "30"];
+ let numberNumber = numberString.map(Number);
+ console.log(numberString);
+ console.log(numberNumber);
+ 
+ function double(number) {
+   return number * 2;
+ }
+ 
+const numbers = [5, 2, 7, 8];
+const doubled = numbers.map(double);
+console.log(doubled);
+
+function isLong(password) {
+  return password.length >= 8;
+}
+
+let password = [
+    "abs176d",
+    " abwqi",
+    " 123456789101122",
+    " 12345678",
+  ];
+let approved = password.filter(isLong);
+console.log(`the passwords that you've tried are ${password}.`);
+console.log(`but only the passwords ${approved} are accepted`);
+
+const data = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle";
+const cities = data.split(",");
+const comma = cities.join(", ");
+
+const dogNames = ["rocket", "bella", "slugger", "flash"];
