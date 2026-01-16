bfa363cd4987d13430f67e13e611a11c0ad2301c
Eric-Alves1 <ericalves.429634@gmail.com>
2026-01-16 12:39:25 -0300

    finishing functions for now i guess

---
+++ javascript/Fundamentals/Functions/Race.js
@@ -0,0 +1,20 @@
+let player1Time = 102;
+let player2Time = 107;
+
+function getFastestRaceTime() {
+  if (player1Time > player2Time) {
+    return player1Time;
+  } else if (player2Time < player1Time) {
+    return player2Time;
+  } else {
+    return player1Time;
+  }
+}
+
+function totalRaceTime() {
+  return player1Time + player2Time;
+}
+
+let totalTime = totalRaceTime();
+let fastestRace = getFastestRaceTime();
+console.log(`the best time was of ${fastestRace} and the total time of the race was ${totalTime}`);
---
+++ javascript/Guided-exercises/Functions/Script.js
@@ -0,0 +1,17 @@
+const names = [
+  "Chris",
+  "Li Kang",
+  "Anne",
+  "Francesca",
+  "Mustafa",
+  "Tina",
+  "Bert",
+  "Jada",
+];
+
+//function isShort(name) {
+//  
+//}
+
+const shortNames = names.filter((name) => name.length < 5);
+console.log(shortNames)
